292b1d03c36028114d7c04b3f1034fca4e4a33c0
shiva <katkamshiva98@gmail.com>
2023-04-01 14:58:50 +0000

    Solution

---
+++ src/components/BrowserHistory/index.js
@@ -0,0 +1,13 @@
+import {Component} from 'react'
+
+class BrowserHistory extends Component {
+  render() {
+    return (
+      <div>
+        <h1>shiva</h1>
+        <p>hema</p>
+      </div>
+    )
+  }
+}
+export default BrowserHistory
